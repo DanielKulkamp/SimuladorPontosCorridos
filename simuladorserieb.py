ca082da6e1b1980bcc7793aa797b3d8293c73d27
DanielKulkamp <daniel.kulkamp@gmail.com>
2021-09-25 22:27:31 -0300

    CFC 1-0 GUA

--- simuladorserieb.py
+++ simuladorserieb.py
@@ -1,5 +1,6 @@
-from random import randint, randrange, random, choice
+from random import randint, randrange, uniform, choice
 import copy
+
 class Time:
     def __init__(self, nome):
         self.rating = 1000.0
@@ -23,15 +24,17 @@ class Time:
         self.hist_gol_fora = []
   
 
+
+
 def match( casa: Time, fora: Time, gcasa: int, gfora: int ):
     casa.gols_pro += gcasa
-    casa.hist_gol_casa.append(gcasa)
-    casa.hist_gol.append(gcasa)
+    #casa.hist_gol_casa.append(gcasa)
+    #casa.hist_gol.append(gcasa)
     casa.gols_contra += gfora
     casa.saldo_gols = casa.gols_pro - casa.gols_contra
     fora.gols_pro += gfora
-    fora.hist_gol_fora.append(gfora)
-    fora.hist_gol.append(gfora)
+    #fora.hist_gol_fora.append(gfora)
+    #fora.hist_gol.append(gfora)
     fora.gols_contra += gcasa
     fora.saldo_gols = fora.gols_pro - fora.gols_contra
     W = 0
@@ -57,7 +60,7 @@ def match( casa: Time, fora: Time, gcasa: int, gfora: int ):
         fora.vitorias += 1
         fora.vitorias_fora += 1
         fora.pontos += 3
-    dr = float(casa.rating - fora.rating)
+    dr = casa.rating - fora.rating
     I = 60
     We = 1./(1.+10.0**(-dr/400.0))
     delta = I * (W-We)
@@ -101,65 +104,70 @@ def simula_ELOHFA(casa: Time, fora: Time):
     else:
         Wdraw = We
     Wb = 1-We
-    results = [] + [(1,0)] *int(100*We)
-    results += [(0,0)] * int(100*Wdraw)
-    results += [(0,1)] * int(100*Wb)
-    (gcasa, gfora) = choice(results)
+    #results = [] + [(1,0)] *int(100*We)
+    #results += [(0,0)] * int(100*Wdraw)
+    #results += [(0,1)] * int(100*Wb)
+    #(gcasa, gfora) = choice(results)
+    alea = uniform(0, We+Wdraw+Wb)
+    if alea < We:
+        (gcasa, gfora) = (1,0)
+    elif alea < We+Wdraw:
+        (gcasa, gfora) = (0, 0)
+    else:
+        (gcasa, gfora) = (0, 1)
+    
     match(casa, fora, gcasa, gfora)
 
+def main():
+    n_simulacoes = 100000
+    algoritmo = simula_ELOHFA
 
-#dic_start = {}
-#for time in times:
-#    dic_start[time] = Time(time)
+    for nth_sim in range(n_simulacoes):
+        if nth_sim % 1000 == 0:
+            print(f'-',end="")
 
+        dictimes = {}
+        for time in times:
+            dictimes[time] = Time(time)
+        jogos_restantes = []
+        with open('TABELA SERIE B.txt', 'r') as arquivo:
+            for line in arquivo.readlines():
+                [ tcasa, placar, tfora] = line.split()
+                if not placar == '-':
+                    casa = dictimes[tcasa]
+                    fora = dictimes[tfora]
+                    [gcasa, gfora] = placar.split('-')
+                    match(casa, fora, int(gcasa), int(gfora))
+                else:
+                    jogos_restantes.append( (tcasa, tfora) )
 
-n_simulacoes = 100000
-algoritmo = simula_ELOHFA
+        #dictimes = copy.deepcopy(dic_start)
 
-for nth_sim in range(n_simulacoes):
-    if nth_sim % 1000 == 0:
-        print(f'-',end="")
+        for (casa, fora) in jogos_restantes:
+            algoritmo(dictimes[casa], dictimes[fora])
 
-    dictimes = {}
+        dictimes['BRU'].pontos -= 3
+
+        times.sort(key= lambda x: dictimes[x].saldo_gols, reverse=True)
+        times.sort(key= lambda x: dictimes[x].vitorias, reverse=True)
+        times.sort(key= lambda x: dictimes[x].pontos, reverse=True)
+
+        dicprobs[times[0]]["titulos"] += 1
+        for team in times[0:4]:
+            dicprobs[team]["acessos"] += 1
+        for team in times[-4:]:
+            dicprobs[team]["rebaixamentos"] +=1
+        
+    times.sort(key= lambda x: dicprobs[x]["rebaixamentos"])
+    times.sort(key= lambda x: dicprobs[x]["acessos"], reverse=True)
+    times.sort(key= lambda x: dicprobs[x]["titulos"], reverse=True)
+    print(f'\nFinal da simulação por {algoritmo.__name__}')
+    print('Time\tTítulo\tSubir\tcair')
+    div = n_simulacoes/100
     for time in times:
-        dictimes[time] = Time(time)
-    jogos_restantes = []
-    with open('TABELA SERIE B.txt', 'r') as arquivo:
-        for line in arquivo.readlines():
-            [ tcasa, placar, tfora] = line.split()
-            if not placar == '-':
-                casa = dictimes[tcasa]
-                fora = dictimes[tfora]
-                [gcasa, gfora] = placar.split('-')
-                match(casa, fora, int(gcasa), int(gfora))
-            else:
-                jogos_restantes.append( (tcasa, tfora) )
-
-    #dictimes = copy.deepcopy(dic_start)
-
-    for (casa, fora) in jogos_restantes:
-        algoritmo(dictimes[casa], dictimes[fora])
-
-    dictimes['BRU'].pontos -= 3
-
-    times.sort(key= lambda x: dictimes[x].saldo_gols, reverse=True)
-    times.sort(key= lambda x: dictimes[x].vitorias, reverse=True)
-    times.sort(key= lambda x: dictimes[x].pontos, reverse=True)
-
-    dicprobs[times[0]]["titulos"] += 1
-    for team in times[0:4]:
-        dicprobs[team]["acessos"] += 1
-    for team in times[-4:]:
-        dicprobs[team]["rebaixamentos"] +=1
-    
-times.sort(key= lambda x: dicprobs[x]["rebaixamentos"])
-times.sort(key= lambda x: dicprobs[x]["acessos"], reverse=True)
-times.sort(key= lambda x: dicprobs[x]["titulos"], reverse=True)
-print(f'\nFinal da simulação por {algoritmo.__name__}')
-print('Time\tTítulo\tSubir\tcair')
-div = n_simulacoes/100
-for time in times:
-    print(f'{time}\t{dicprobs[time]["titulos"]/div:.2f}\t{dicprobs[time]["acessos"]/div:.2f}\t{dicprobs[time]["rebaixamentos"]/div:.2f}')
+        print(f'{time}\t{dicprobs[time]["titulos"]/div:.2f}\t{dicprobs[time]["acessos"]/div:.2f}\t{dicprobs[time]["rebaixamentos"]/div:.2f}')
 
 
 
+import cProfile
+cProfile.run('main()')
